e260cf97272469fe141d3be081f71f4a9cd2a427
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-07-13 12:58:23 +0300

    Challenge2 added

---
+++ Lectures/03-Functions/Functions/12-Challenge2/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+void print_menu();
+void input_to_upper();
+void print_numbers();
+void add_number();
+void calculate_mean();
+void display_smallest_num();
+void display_largest_num();
+void quit();
+
+int main()
+{
+
+    std::cout<<"Template"<<std::endl;
+
+
+	return 0;
+}
+
+void print_menu(){
+
+}
+void input_to_upper(){
+
+}
+
+void print_numbers(){
+
+}
+
+void add_number(){
+
+}
+
+void calculate_mean(){
+
+}
+
+void display_smallest_num(){
+
+}
+
+void display_largest_num(){
+
+}
+
+void quit(){
+
+}
