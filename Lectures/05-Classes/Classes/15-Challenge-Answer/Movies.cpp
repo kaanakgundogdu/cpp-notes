c905876611afc5d4fbd4cd74bff28ea3a5dab199
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-07-21 13:31:27 +0300

    Challenge answer file created.

---
+++ Lectures/05-Classes/Classes/15-Challenge-Answer/Movies.cpp
@@ -0,0 +1,21 @@
+#include "Movies.h"
+
+Movies::Movies()
+{
+}
+
+Movies::~Movies()
+{
+}
+
+bool Movies::add_movie(std::string name, std::string rating, int watched){
+    return false;
+}
+
+bool Movies::increment_watched(std::string name){
+    return false;
+}
+
+void Movies::display() const{
+
+}
