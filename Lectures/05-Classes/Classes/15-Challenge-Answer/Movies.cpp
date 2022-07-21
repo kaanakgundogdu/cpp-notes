659c929dee62bce2c4cb1cf8eb6c62e066bd1742
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-07-21 14:18:10 +0300

    Challenge finished.

--- Lectures/05-Classes/Classes/15-Challenge-Answer/Movies.cpp
+++ Lectures/05-Classes/Classes/15-Challenge-Answer/Movies.cpp
@@ -1,4 +1,5 @@
 #include "Movies.h"
+#include <iostream>
 
 Movies::Movies()
 {
@@ -9,13 +10,35 @@ Movies::~Movies()
 }
 
 bool Movies::add_movie(std::string name, std::string rating, int watched){
-    return false;
+    for(const Movie &movie : movies){
+        if(movie.get_name()==name)
+            return false;
+    }
+    Movie temp {name,rating,watched};
+    movies.push_back(temp);
+    return true;
 }
 
 bool Movies::increment_watched(std::string name){
+    for( Movie &movie : movies){
+        if(movie.get_name()==name){
+            movie.increment_watched();
+            return true;
+        }
+    }
     return false;
 }
 
 void Movies::display() const{
+    if(movies.size()==0){
+        std::cout<<"Sorry no movies to display"<<std::endl;
+    }
+    else{
+        std::cout<<"\n---------------------------------"<<std::endl;
+        for(Movie movie : movies){
+            movie.display();
+        }
 
+        std::cout<<"\n---------------------------------"<<std::endl;
+    }
 }
