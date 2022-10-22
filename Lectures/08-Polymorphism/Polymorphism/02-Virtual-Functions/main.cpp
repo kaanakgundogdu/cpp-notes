54c05d49b048f22ee0883fa66a1bab1c6f58a9ce
Kaan Akgundogdu <kaanakgundogdu@gmail.com>
2022-10-22 20:32:19 +0300

    Virtual Destructors added.

--- Lectures/08-Polymorphism/Polymorphism/02-Virtual-Functions/main.cpp
+++ Lectures/08-Polymorphism/Polymorphism/02-Virtual-Functions/main.cpp
@@ -5,12 +5,18 @@ public:
     virtual void withdraw(double amount){
         std::cout<<"In Account::withdraw"<<std::endl;
     }
+    virtual ~Account(){
+        std::cout<<"Account::destructor"<<std::endl;
+    }
 };
 
 class Checking:public Account{
 public:
     virtual void withdraw(double amount){
         std::cout<<"In Checking::withdraw"<<std::endl;
+    }
+    virtual ~Checking(){
+        std::cout<<"Checking::destructor"<<std::endl;
     }
 };
 
@@ -19,12 +25,18 @@ public:
     virtual void withdraw(double amount){
         std::cout<<"In Savings::withdraw"<<std::endl;
     }
+    virtual ~Savings(){
+        std::cout<<"Savings::destructor"<<std::endl;
+    }
 };
 
 class Trust: public Account{
 public:
     virtual void withdraw(double amount){
         std::cout<<"In Trust::withdraw"<<std::endl;
+    }
+    virtual ~Trust(){
+        std::cout<<"Trust::destructor"<<std::endl;
     }
 };
 
