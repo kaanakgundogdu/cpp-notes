8b233cb03e0c4ce0758f6cad3d76c0cda2ae644d
Kaan Akgundogdu <kaanakgundogdu@gmail.com>
2022-10-28 11:31:57 +0300

    Weak pointers added.

---
+++ Lectures/09-Smart-Pointers/Smart-Pointers/03-Weak-Pointers/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <memory>
+
+class B;
+ 
+class A{
+  std::shared_ptr<B> b_ptr;
+public:
+    void set_B(std::shared_ptr<B> &b){
+    b_ptr=b;
+    };
+    A(){std::cout<<"A Constructor"<<std::endl;}
+    ~A(){std::cout<<"A Destructor"<<std::endl;}
+};
+
+class B{
+    //shared pointer causes memory leak so we must use weak ptr
+    /*std::shared_ptr<A> a_ptr;*/ 
+    std::weak_ptr<A> a_ptr;
+public:
+    void set_A(std::shared_ptr<A> &a){
+        a_ptr=a;
+    }
+    B(){std::cout<<"B Constructor"<<std::endl;}
+    ~B(){std::cout<<"B Destructor"<<std::endl;}
+    
+};
+
+int main()
+{
+    std::shared_ptr<A> a = std::make_shared<A>();
+    std::shared_ptr<B> b = std::make_shared<B>();
+    a->set_B(b);
+    b->set_A(a);
+    
+        
+	return 0;
+}
